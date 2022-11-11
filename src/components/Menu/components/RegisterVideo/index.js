706e62e4aaf8a17b85fe4d531c61357d57da3eac
dhdessoldi <110476564+dhdessoldi@users.noreply.github.com>
2022-11-11 13:55:10 -0300

    início do formulario

---
+++ src/components/Menu/components/RegisterVideo/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { StyledRegisterVideo } from "./styles";
+
+export default function RegisterVideo() {
+    const [formVisivel, setFormVisivel] = React.useState(false);
+    return (
+        <StyledRegisterVideo>
+            <button className="add-video" onClick={()=>setFormVisivel(true)}>
+                +
+            </button>
+            {formVisivel
+                ? (
+                    <form>
+                        <div>
+
+                            <button className="close-modal" onClick={()=>setFormVisivel(false)}>
+                                X
+                            </button>
+                            <input placeholder="Título do vídeo" />
+                            <input placeholder="URL do vídeo" />
+                            <button type="submit">
+                                Cadastrar
+                            </button>
+                        </div>
+                    </form>
+                )
+                : false}
+        </StyledRegisterVideo>
+    )
+}
+
